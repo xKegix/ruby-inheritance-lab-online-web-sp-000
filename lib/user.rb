15afa7a0bd83d26d00f2ebc1475d812f0d0921b8
Keginold Philip <keginoldp@hotmail.com>
2021-02-27 14:47:41 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -3,7 +3,7 @@ class User
   attr_accessor :firstname, :lastname
 
   def initialize
-    @first_name = firstname
-    @last_name = lastname
+    @first_name = first_name
+    @last_name = last_name
   end
 end
